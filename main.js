a51a3d709f980897d1255e417d2d5f6e1b6edca9
Tricia <totricia@gmail.com>
2017-02-12 21:49:44 -0800

    Added a logo!

--- main.js
+++ main.js
@@ -4,13 +4,22 @@ import {
   StyleSheet,
   Text,
   View,
+  Image,
 } from 'react-native';
 
 class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
+        <View style={styles.header}>
+          <Image
+            source={require('./images/mrw.png')}
+            style={styles.mrwLogo} />
+          <Text>Hello there!</Text>
+        </View>
+        <View style={styles.playArea}>
+          <Text>Open up main.js to start working on your app!</Text>
+        </View>
       </View>
     );
   }
@@ -20,9 +29,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+    flexDirection: 'column',
   },
+  header: {
+    padding: 20,
+    paddingTop: 40,
+    alignItems: 'center',
+    height: 130,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  mrwLogo: {
+    width: 170,
+    height: 100,
+  },
+  playArea: {
+    padding: 20,
+    flex: 1,
+    justifyContent: 'center',
+  }
 });
 
 Exponent.registerRootComponent(App);
