74ff0ed37c2441e22e131573eece5ea27ffda927
Tricia <totricia@gmail.com>
2017-06-26 23:21:54 -0700

    Remove HTTP requests from Sentry autobreadcrumbs

--- main.js
+++ main.js
@@ -2,5 +2,9 @@ import Expo from 'expo';
 import App from './javascripts/App';
 import Sentry from 'sentry-expo';
 
-Sentry.config('https://dd4470cda4ed444bb6700eaf2eaf3c3b@sentry.io/182829').install();
+Sentry.config('https://dd4470cda4ed444bb6700eaf2eaf3c3b@sentry.io/182829', {
+  autoBreadcrumbs: {
+    xhr: false
+  }
+}).install();
 Expo.registerRootComponent(App);
